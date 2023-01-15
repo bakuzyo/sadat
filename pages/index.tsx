1dbea8d249d98b57f80d791e06f25d16e4afb59a
Valentin Batiot <valentin.batiot@inli.fr>
2023-01-15 17:23:50 +0100

    ImportModification

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import type { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
-import About from '../components/About'
-import Competences from '../components/Competences'
-import Diplomes from '../components/Diplomes'
-import WorkExperience from '../components/WorkExperience'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import MessageMe from '../components/MessageMe'
+// import About from '../components/About'
+// import Competences from '../components/Competences'
+// import Diplomes from '../components/Diplomes'
+// import WorkExperience from '../components/WorkExperience'
+// import Header from '../components/Header'
+// import Hero from '../components/Hero'
+// import MessageMe from '../components/MessageMe'
 import { Experience, PageInfo, Project, Skill, Social } from "../typings";
 import { fetchPageInfo } from '../utils/fetchPageInfo'
 import { fetchExperiences } from '../utils/fetchExperiences'
@@ -26,31 +26,31 @@ type Props = {
 
 const Home = ({ pageInfo, experiences, projects, skills, socials}: Props ) => {
   
-  // const Diplomes = dynamic(() => import('../components/Diplomes'), {
-  //   ssr: false,
-  // })
+  const Diplomes = dynamic(() => import('../components/Diplomes'), {
+    ssr: false,
+  })
 
-  // const Competences = dynamic(() => import('../components/Competences'), {
-  //   ssr: false,
-  // })
+  const Competences = dynamic(() => import('../components/Competences'), {
+    ssr: false,
+  })
 
-  // const WorkExperience = dynamic(() => import('../components/WorkExperience'), {
-  //   ssr: false,
-  // })
+  const WorkExperience = dynamic(() => import('../components/WorkExperience'), {
+    ssr: false,
+  })
 
-  // const Header = dynamic(() => import('../components/Header'), {
-  //   ssr: false,
-  // })
-  // const Hero = dynamic(() => import('../components/Hero'), {
-  //   ssr: false,
-  // })
+  const Header = dynamic(() => import('../components/Header'), {
+    ssr: false,
+  })
+  const Hero = dynamic(() => import('../components/Hero'), {
+    ssr: false,
+  })
 
-  // const MessageMe = dynamic(() => import('../components/MessageMe'), {
-  //   ssr: false,
-  // })
-  // const About = dynamic(() => import('../components/About'), {
-  //   ssr: false,
-  // })
+  const MessageMe = dynamic(() => import('../components/MessageMe'), {
+    ssr: false,
+  })
+  const About = dynamic(() => import('../components/About'), {
+    ssr: false,
+  })
 
   return (
     <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory
